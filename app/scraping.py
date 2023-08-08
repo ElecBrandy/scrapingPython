e88be2c517dd3e5acfed2432249dc9b3a3a62340
elecbrandy <elecbrandy@gmail.com>
2023-08-09 06:02:17 +0900

    자막 전까지

--- app/scraping.py
+++ app/scraping.py
@@ -1,11 +1,18 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from bs4 import BeautifulSoup
+from youtube_transcript_api import YouTubeTranscriptApi
 import pandas as pd
+from tabulate import tabulate
 import time
+import re
 import os
 
-url = "https://www.youtube.com/watch?v=RZupmCwrwec"
+# tabulate 한글깨짐 방지
+# tabulate.WIDE_CHARS_MODE = False
 
+# ------------------------------------ ↓↓↓ Selenium Firefox 설정 ↓↓↓ ------------------------------------ #
 # headless mode 사용
 options = webdriver.FirefoxOptions()
 options.headless = True
@@ -25,22 +32,77 @@ os.system(xvfb_cmd)
 
 # 파이어폭스 드라이브 시작
 driver = webdriver.Firefox(options=options)
-
-# 페이지로 이동
-driver.get(url)
+# ------------------------------------ ↑↑↑ Selenium Firefox 설정 ↑↑↑ ------------------------------------ #
 
 
-# 딜레이 
-time.sleep(2)
-driver.find_element(By.XPATH, "/html/body/ytd-app/div[1]/ytd-page-manager/ytd-watch-flexy/div[5]/div[1]/div/div[2]/ytd-watch-metadata/div/div[2]/div[2]/div/div/ytd-menu-renderer/yt-button-shape/button/yt-touch-feedback-shape/div").click()
-time.sleep(5)
 
-# # 2. '스크립트 표시'라는 텍스트를 가진 버튼 클릭
-driver.find_element(By.CSS_SELECTOR, "html body ytd-app ytd-popup-container.style-scope.ytd-app tp-yt-iron-dropdown.style-scope.ytd-popup-container div#contentWrapper.style-scope.tp-yt-iron-dropdown ytd-menu-popup-renderer.style-scope.ytd-popup-container tp-yt-paper-listbox#items.style-scope.ytd-menu-popup-renderer ytd-menu-service-item-renderer.style-scope.ytd-menu-popup-renderer").click()
+# ------------------------------------ ↓↓↓ 함수 설정 ↓↓↓ ------------------------------------ #
 
-# file = open("test.txt", "w")
-print(driver.find_element(By.XPATH, "/html/body/ytd-app/div[1]/ytd-page-manager/ytd-watch-flexy/div[5]/div[1]/div/div[2]/ytd-watch-metadata/div/div[2]/div[2]/div/div/ytd-menu-renderer/yt-button-shape/button/yt-touch-feedback-shape/div"))
-driver.quit()
+def getSubtitle(ID):
+    # 동영상의 자막 정보 조회
+    transcript_list = YouTubeTranscriptApi.list_transcripts(ID)
+
+    # 한국어 (ko) 자막 가져오기
+    korean_transcript = transcript_list.find_transcript(['ko']).fetch()
+
+    # korean_transcript 변수에 한국어 자막 내용 저장
+    print(korean_transcript)
+
+def searchKeywords(keyword, data_dict):
+    # 키워드 공백 처리 + url 생성
+    text = keyword.replace(' ', '+')
+    url = "https://www.youtube.com/results?search_query=" + text
+    print("현재 사이트 url : %s", url)
+
+    # url 접근 + 딜레이(3)
+    driver.get(url)
+    time.sleep(3)
+    # 페이지 소스 추출 : selenium으로 html 수집, bs4로 파싱
+    html_source = driver.page_source
+    soup_source = BeautifulSoup(html_source, 'html.parser')
+
+    # 쇼츠 정보 삭제
+    soup_source.find('ytd-reel-shelf-renderer', class_='style-scope ytd-item-section-renderer').decompose()
+    
+    # 콘텐츠 모든 정보
+    content_total = soup_source.find_all(class_ = 'yt-simple-endpoint style-scope ytd-video-renderer')
+    # 콘텐츠 제목만 추출
+    content_total_title = list(map(lambda data: data.get_text().replace("\n", ""), content_total))
+    # 콘텐츠 링크만 추출
+    content_total_link = list(map(lambda data: "https://youtube.com" + data["href"], content_total))
+    # 조회수 및 업로드 날짜 추가
+    content_record_src = soup_source.find_all(class_ = 'inline-metadata-item style-scope ytd-video-meta-block')
+    
+    content_view_cnt = [record.get_text() for record in content_record_sr[::2]]
+    content_upload_date = [record.get_text() for record in content_record_src[1::2]]
+
+    # 데이터 딕셔너리에 추가
+    for title, link, view, upload_date in zip(content_total_title, content_total_link, content_view_cnt, content_upload_date):
+        if "https://youtube.com/shorts" in link:
+            continue  # shorts 링크인 경우 건너뜀
+        data_dict['title'].append(title)
+        data_dict['link'].append(link)
+        data_dict['view'].append(view)
+        data_dict['upload_date'].append(upload_date)
+
+    df = pd.DataFrame(data_dict)
+    return df
+
+# ------------------------------------ ↑↑↑ 함수 설정 ↑↑↑ ------------------------------------ #
+
+# 빈 딕셔너리 생성
+content_total_dict = {'title': [], 'link': [], 'view': [], 'upload_date': []}
+
+t = "아이폰"
+tt = searchKeywords(t, content_total_dict)
+# print(tabulate(tt, headers='keys', tablefmt='fancy_outline'))
+# CSV 파일로 저장
+
+csv_filename = 'output.csv'
+tt.to_csv(csv_filename, index=True, index_label='row_data')
+
+print(f"데이터가 {csv_filename} 파일로 저장되었습니다.")
+
 # # 4. 스크립트만 추출하기
 # elements = driver.find_elements(By.CSS_SELECTOR, "html body ytd-app div#content.style-scope.ytd-app ytd-page-manager#page-manager.style-scope.ytd-app ytd-watch-flexy.style-scope.ytd-page-manager.hide-skeleton div#columns.style-scope.ytd-watch-flexy div#secondary.style-scope.ytd-watch-flexy div#secondary-inner.style-scope.ytd-watch-flexy div#panels.style-scope.ytd-watch-flexy ytd-engagement-panel-section-list-renderer.style-scope.ytd-watch-flexy")
 # try:
--- app/youtube.py
+++ app/youtube.py
@@ -1,15 +1,15 @@
 from youtube_transcript_api import YouTubeTranscriptApi
 
-def Subtitle(ID):
-    # 동영상의 자막 정보를 조회
+def getSubtitle(ID):
+    # 동영상의 자막 정보 조회
     transcript_list = YouTubeTranscriptApi.list_transcripts(ID)
 
-    # 한국어 (ko) 자막을 가져오기~
+    # 한국어 (ko) 자막 가져오기
     korean_transcript = transcript_list.find_transcript(['ko']).fetch()
 
-    # korean_transcript 변수에 한국어 자막 내용이 저장~!
+    # korean_transcript 변수에 한국어 자막 내용 저장
     print(korean_transcript)
 
-
-ID1 = 'mdNZI3TKR0I'
-Subtitle(ID1)
+# ID1 = 'mdNZI3TKR0I'
+# getSubtitle(ID1)
+# arrWord = []
